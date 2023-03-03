a18a688aefc7614888b493bc80a3126c7dab1e7b
PabloGM1204 <115071457+PabloGM1204@users.noreply.github.com>
2023-03-03 11:10:52 +0100

    Delete src/main directory

--- src/main/webapp/assets/JS/reglas.js
+++
@@ -1,6 +0,0 @@
-var button = document.querySelector('.toggle-button');
-var content = document.querySelector('.content');
-
-button.addEventListener('click', function() {
-  content.classList.toggle('open');
-});
